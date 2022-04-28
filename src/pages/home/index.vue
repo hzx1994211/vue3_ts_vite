fe2d2fbe9d9a1067b4b2b3c434025b52fd51403c
huangzhenxiang <1442140320@qq.com>
2022-04-28 16:40:00 +0800

    modify:home新增静态表格

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,26 +2,72 @@
  * @Author: huangzhenxiang
  * @Date: 2022-04-22 14:44:07
  * @LastEditors: huangzhenxiang
- * @LastEditTime: 2022-04-26 14:22:55
+ * @LastEditTime: 2022-04-28 16:34:17
 -->
 <template>
   <div>
-    <div>首页11111</div>
-    <input list="listName" class="list">
-    <datalist id="listName">
-      <option value="value1" />
-      <option value="value2" />
-      <option value="value3" />
-      <option value="value4" />
-      <option value="value5" />
-    </datalist>
     <div>{{num}}</div>
     <div @click="btnClick">点击事件</div>
+    <div>
+      <div class="home-title">
+        <div class="home-title-left">
+          左边
+        </div>
+        <div class="home-title-right">
+          右边
+        </div>
+      </div>
+      <div class="home-main">
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column prop="date" label="Date" width="180" />
+          <el-table-column prop="name" label="Name" width="180" />
+          <el-table-column prop="address" label="Address" />
+        </el-table>
+        <div class="home-pagination-block">
+          <el-pagination v-model:currentPage="currentPage4" v-model:page-size="pageSize4" :page-sizes="[100, 200, 300, 400]" :small="small" :disabled="disabled" :background="background" layout="total, sizes, prev, pager, next, jumper" :total="400" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
 import { Ref, ref, reactive } from "vue";
-let num = ref(0);
+const data = [
+  {
+    date: "2016-05-03",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-02",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-04",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-01",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+];
+const tableData = reactive(data);
+const num = ref(0);
+const currentPage4 = ref(4);
+const pageSize4 = ref(100);
+const small = ref(false);
+const background = ref(false);
+const disabled = ref(false);
+
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`);
+};
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`);
+};
 function btnClick() {
   num.value++;
   console.log(num, "----num");
@@ -29,7 +75,14 @@ function btnClick() {
 </script>
 
 <style lang="scss" scoped>
-.list {
-  border: 1px solid rgb(118, 118, 118);
+.home-title {
+  display: flex;
+  justify-content: space-between;
+  & > div {
+    flex: 1;
+  }
+}
+.home-pagination-block + .home-pagination-block {
+  margin-top: 10px;
 }
 </style>
