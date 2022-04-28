52a5b7f53f3bd282f3b7c2e98ebe4cb75beb21c8
huangzhenxiang <1442140320@qq.com>
2022-04-28 11:49:23 +0800

    fix:修复package报错

--- src/layout/components/UserInfo/index.vue
+++ src/layout/components/UserInfo/index.vue
@@ -2,11 +2,11 @@
  * @Author: huangzhenxiang
  * @Date: 2022-04-28 11:36:58
  * @LastEditors: huangzhenxiang
- * @LastEditTime: 2022-04-28 11:40:39
+ * @LastEditTime: 2022-04-28 11:48:09
 -->
 <template>
   <div class="user">
-    <div>{{user}}</div>
+    <div>{{user.name}}</div>
   </div>
 </template>
 
