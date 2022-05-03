6f30bd25582c07e5e4dcb7cd5effc1213edb3f78
huangzhenxiang <1442140320@qq.com>
2022-05-03 15:08:15 +0800

    delete:删除无用type

--- src/api/login/index.ts
+++ src/api/login/index.ts
@@ -2,22 +2,10 @@
  * @Author: huangzhenxiang
  * @Date: 2022-04-18 17:23:38
  * @LastEditors: huangzhenxiang
- * @LastEditTime: 2022-04-29 15:51:03
+ * @LastEditTime: 2022-05-03 15:07:25
  */
 //@ts-ignore
 import http from '@/service/request'
-// import * as T from './types'
-// const accountLogin: T.ILoginApi = {
-//     login(params){
-//         return http.post('/login', params)
-//     }
-
-// }
-// const userInfoRequest:T.UserInfoApi = {
-//   userInfo(){
-//       return http.post('/login')
-//   }
-// }
 //用户登陆
 const accountLogin = (params:any) => http.post('users/login', params)
 //获取用户详情
