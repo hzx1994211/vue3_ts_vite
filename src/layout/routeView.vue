ab5274d7d924683e1d9b17f33430e4ba992a3ad6
huangzhenxiang <1442140320@qq.com>
2022-08-16 09:58:05 +0800

    feat:新增keep alice注释

--- src/layout/routeView.vue
+++ src/layout/routeView.vue
@@ -2,16 +2,29 @@
  * @Author: huangzhenxiang
  * @Date: 2022-08-08 17:55:43
  * @LastEditors: huangzhenxiang
- * @LastEditTime: 2022-08-08 17:55:45
+ * @LastEditTime: 2022-08-16 09:57:36
 -->
 <template>
   <router-view />
+  <!-- 由于keep-alive是根据script的name来缓存， cli5.0脚手架不好配置script的组件name 所以该方法暂注释 
+        但是vite可以， 不过需要下载插件 npm i vite-plugin-vue-setup-extend -D
+        https://www.cnblogs.com/huoshengmiao/p/15964628.html
+  -->
   <!-- <router-view #default="{ Component }">
     <transition appear name="fade-transform" mode="out-in">
       <keep-alive :include="includeList.value">
         <component :is="Component" />
       </keep-alive>
     </transition>
+  </router-view> -->
+
+  <!-- 缓存 -->
+  <!-- :key="$route.path" :include="includeList.value" :exclude="excludeList.value" -->
+  <!-- <router-view #default="{ Component }">
+    <keep-alive>
+      <component :is="Component" :key="$route.path" v-if="$route.meta.keepAlive" />
+    </keep-alive>
+    <component :is="Component" :key="$route.path" v-if="!$route.meta.keepAlive" />
   </router-view> -->
 </template>
 <script lang="ts" setup>
